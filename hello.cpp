b3b229bc32329ab652996fe46f7763197b51f794
yogi7774 <yogesh.waghmare.aids.2023@vpkbiet.org>
2026-01-05 09:45:50 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout << "The number is Even" << endl;
+    else
+        cout << "The number is Odd" << endl;
+
     return 0;
 }
